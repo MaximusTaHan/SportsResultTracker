c9c5f1956106ad68fe62dfde2d4bd0ee08f9e677
Max <Max.95hansen@gmail.com>
2022-05-02 10:24:50 +0200

    Add project files.

---
+++ SportsResultTracker/BasketballStatsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsResultTracker
+{
+    internal class BasketballStatsModel
+    {
+        public string TeamName { get; set; }
+        public string Wins { get; set; }
+        public string Loses { get; set; }
+        public string WinLosePrecent { get; set; }
+        public string GamesBehind { get; set; }
+        public string PointsPerGame { get; set; }
+        public string OpponentPointsPerGame { get; set; }
+    }
+}
---
+++ SportsResultTracker/Program.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+using SportsResultTracker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HtmlWeb web = new();
+        HtmlDocument doc = web.Load("https://www.basketball-reference.com/boxscores/");
+        List<BasketballStatsModel> statsList = new List<BasketballStatsModel>();
+
+        var title = doc.DocumentNode.SelectNodes("//*[@id=\"confs_standings_E_sh\"]/h2").First().InnerText;
+        var tableData = doc.DocumentNode.SelectNodes("//*[@id=\"confs_standings_E\"]/tbody/tr");
+        
+        Console.WriteLine(title);
+        tableData.ToList().ForEach(t => statsList.Add(new BasketballStatsModel
+        {
+            TeamName = t.ChildNodes[0].InnerText,
+            Wins = t.ChildNodes[1].InnerText,
+            Loses = t.ChildNodes[2].InnerText,
+            WinLosePrecent = t.ChildNodes[3].InnerText,
+            GamesBehind = t.ChildNodes[4].InnerText,
+            PointsPerGame = t.ChildNodes[5].InnerText,
+            OpponentPointsPerGame = t.ChildNodes[6].InnerText
+        }));
+
+        foreach (var item in statsList)
+        {
+            Console.WriteLine($"Team Name: {item.TeamName}, Wins: {item.Wins}, Loses: {item.Loses}, W/L%: {item.WinLosePrecent}, GB: {item.GamesBehind}, PS/G: {item.PointsPerGame}, PA/G: {item.OpponentPointsPerGame}");
+        }
+    }
+}
